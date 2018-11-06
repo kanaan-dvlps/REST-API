1368efc7beafd56814e52ee84e9ec300035b520f
kanan bahmani <33728936+kansci@users.noreply.github.com>
2018-11-06 17:10:00 +0330

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 3000,
-  URL: process.env.BASE_URL || 'http://127.0.0.1: 3000',
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://rest_api:compl3x!ty@ds249123.mlab.com:49123/rest_api',
-  JWT_SECRET: process.env.JWT_SECRET || 'secret'
-}
